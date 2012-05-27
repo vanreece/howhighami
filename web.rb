f0d043feeca5af3b04c51f3e15409270024220ff
Danny Krause <danny@carbonfive.com>
2012-05-27 10:49:51 -0700

    Serve javascript files from public. Rename currentElevation.html to howhighami.html

--- web.rb
+++ web.rb
@@ -1,13 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  File.open("currentElevation.html").read
-end
-
-get '/jquery-1.7.2.js' do
-  File.open("jquery-1.7.2.js").read
-end
-
-get '/jquery.xdomainajax.js' do
-  File.open("jquery.xdomainajax.js").read
+  File.open('howhighami.html').read
 end
